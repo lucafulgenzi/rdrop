9221c7dc47d98a34ceb0f00b0344934389317822
Luca Fulgenzi <fulgenzi.luca@gmail.com>
2025-10-30 14:18:46 +0100

    refactor code;

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
 use dirs::home_dir;
 use serde::{Deserialize, Serialize};
+use std::error::Error;
 use std::io;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use twelf::reexports::log;
 use twelf::{config, Layer};
 
 #[derive(Parser, Debug)]
@@ -36,8 +38,22 @@ struct Config {
     position: TermPosition,
 }
 
-
 #[derive(Deserialize)]
+struct TerminalSize {
+    width: i32,
+    height: i32,
+}
+
+impl Default for TerminalSize {
+    fn default() -> Self {
+        Self {
+            width: 600,
+            height: 400,
+        }
+    }
+}
+
+#[derive(Deserialize, PartialEq)]
 struct Workspace {
     id: i32,
     name: String,
@@ -50,14 +66,13 @@ struct Monitor {
     focused: bool,
 }
 
-/// Define struct for hyprctl client
 #[derive(Deserialize)]
 struct Client {
     class: String,
     workspace: Workspace,
 }
 
-// -- Configs functions --
+// --- Configs functions ---
 
 fn default_config_path() -> String {
     home_dir()
@@ -69,14 +84,15 @@ fn default_config_path() -> String {
         .unwrap_or_else(|| "/tmp/rdrop.yaml".to_string())
 }
 
-fn load_configs(path: PathBuf) -> Result<Config, Box<dyn std::error::Error>> {
+fn load_configs(path: PathBuf) -> Result<Config, Box<dyn Error>> {
     let path = path.into();
     let conf = Config::with_layers(&[Layer::Yaml(path)])?;
     Ok(conf)
 }
 
-/// -- Helper functions --
+/// --- Helper functions for hyprctl commands ---
 
+/// Exec external command and return the output in JSON
 fn get_json_output(cmd: &str, args: &[&str]) -> io::Result<String> {
     let output = Command::new(cmd)
         .args(args)
@@ -86,13 +102,17 @@ fn get_json_output(cmd: &str, args: &[&str]) -> io::Result<String> {
 
     if !output.status.success() {
         let err = String::from_utf8_lossy(&output.stderr);
-        return Err(io::Error::new(io::ErrorKind::Other, err.to_string()));
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("Failed to exec command: {} {}", cmd, err),
+        ));
     }
 
     Ok(String::from_utf8_lossy(&output.stdout).to_string())
 }
 
-fn dispatch_hyrpctl_command(args: &[&str]) -> io::Result<String> {
+/// Exec hyprctl dispatch with args
+fn dispatch_hyrpctl_command(args: &[&str]) -> io::Result<()> {
     let output = Command::new("hyprctl")
         .arg("dispatch")
         .arg("--")
@@ -101,178 +121,225 @@ fn dispatch_hyrpctl_command(args: &[&str]) -> io::Result<String> {
         .stderr(Stdio::piped())
         .output()?;
 
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    println!("{}", stdout);
-
     if !output.status.success() {
         let err = String::from_utf8_lossy(&output.stderr);
-        return Err(io::Error::new(io::ErrorKind::Other, err.to_string()));
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!(
+                "Failed to dispatch hyprctl command (args: {:?}): {}",
+                args, err
+            ),
+        ));
     }
-    Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    Ok(())
 }
 
-fn get_clients() -> Result<Vec<Client>, Box<dyn std::error::Error>> {
+// --- Helper functions for get hyprclt env ---
+fn get_clients() -> Result<Vec<Client>, Box<dyn Error>> {
     let out = get_json_output("hyprctl", &["-j", "clients"])?;
     let clients: Vec<Client> = serde_json::from_str(&out)?;
     Ok(clients)
 }
 
-fn get_active_workspace() -> Result<Workspace, Box<dyn std::error::Error>> {
+fn get_active_workspace() -> Result<Workspace, Box<dyn Error>> {
     let out = get_json_output("hyprctl", &["-j", "activeworkspace"])?;
     let ws: Workspace = serde_json::from_str(&out)?;
     Ok(ws)
 }
 
-fn get_monitors() -> Result<Vec<Monitor>, Box<dyn std::error::Error>> {
+fn get_monitors() -> Result<Vec<Monitor>, Box<dyn Error>> {
     let out = get_json_output("hyprctl", &["-j", "monitors"])?;
     let monitors: Vec<Monitor> = serde_json::from_str(&out)?;
     Ok(monitors)
 }
 
-fn find_terminal(class: &String) -> Option<Client> {
-    let clients = match get_clients() {
-        Ok(clients) => clients,
-        Err(e) => {
-            eprintln!("No clients found: {}", e);
-            return None;
-        }
-    };
-
-    clients.into_iter().find(|c| c.class.eq(class))
+fn find_terminal(class: &String) -> Result<Option<Client>, Box<dyn Error>> {
+    let clients = get_clients()?;
+    Ok(clients.into_iter().find(|c| c.class.eq(class)))
 }
 
 /// Get focused monitor from hyprctl
-fn find_active_monitor() -> Option<Monitor> {
-    let monitors = match get_monitors() {
-        Ok(monitors) => monitors,
-        Err(e) => {
-            eprintln!("No monitor found: {}", e);
-            return None;
+fn find_active_monitor() -> Result<Monitor, Box<dyn Error>> {
+    let monitors = get_monitors()?;
+
+    monitors
+        .into_iter()
+        .find(|m| m.focused)
+        .ok_or_else(|| "No focused monitor found".into())
+}
+
+// --- Calcs logics and dispatch ---
+
+/// Calculate TerminalSize from monitor sizes and configs percentages
+fn calc_terminal_size_from_percentage(
+    width_percent: i32,
+    height_percent: i32,
+    monitor: &Monitor,
+) -> TerminalSize {
+    let terminal_width = (monitor.width * width_percent) / 100;
+    let terminal_height = (monitor.height * height_percent) / 100;
+    TerminalSize {
+        width: terminal_width,
+        height: terminal_height,
+    }
+}
+
+fn dispatch_terminal_positioning(
+    class: &str,
+    monitor: &Monitor,
+    position: &TermPosition,
+    gap: i32,
+    terminal_size: &TerminalSize,
+) -> Result<(), Box<dyn Error>> {
+    let (x_calc, y_calc) = match position {
+        TermPosition::T => {
+            let x = (monitor.width - terminal_size.width) / 2;
+            let y = gap;
+            (x, y)
+        }
+        TermPosition::R => {
+            let x = monitor.width - (terminal_size.width + gap);
+            let y = (monitor.height - terminal_size.height) / 2;
+            (x, y)
+        }
+        TermPosition::B => {
+            let x = (monitor.width - terminal_size.width) / 2;
+            let y = monitor.height - (terminal_size.height + gap);
+            (x, y)
+        }
+        TermPosition::L => {
+            let x = gap;
+            let y = (monitor.height - terminal_size.height) / 2;
+            (x, y)
         }
     };
-    monitors.into_iter().find(|m| m.focused)
+
+    let x_pos = x_calc.to_string();
+    let y_pos = y_calc.to_string();
+    let class_arg = format!("class:{}", class);
+
+    let command_args: Vec<&str> = vec![
+        "movewindowpixel",
+        "exact",
+        x_pos.as_str(),
+        y_pos.as_str(),
+        ",",
+        &class_arg,
+    ];
+
+    dispatch_hyrpctl_command(&command_args)?;
+    Ok(())
 }
 
-// TODO: add validation for width and height min 0, max 100
-fn calc_terminal_percentage_size(width: i32, height: i32) -> (i32, i32) {
-    let active_monitor = find_active_monitor();
-    if active_monitor.is_none() {
-        println!("No monitor found");
-        return (-1, -1);
-    }
-
-    let monitor = active_monitor.unwrap();
-
-    let terminal_width = (monitor.width * width) / 100;
-    let terminal_height = (monitor.height * height) / 100;
-
-    (terminal_width, terminal_height)
+fn dispatch_terminal_init(terminal: &str, class: &str) -> Result<(), Box<dyn Error>> {
+    let command_args: Vec<&str> = vec![
+        "exec",
+        "[workspace special:rdrop silent;float;]",
+        terminal,
+        "--class",
+        class,
+    ];
+    dispatch_hyrpctl_command(&command_args)?;
+    Ok(())
 }
 
+fn dispatch_terminal_move(class: &str, terminal_workspace: i32) -> Result<(), Box<dyn Error>> {
+    let class_arg = format!("class:{}", class);
 
+    let ws = get_active_workspace()?;
 
-fn parse_commands(config: &Config, create: bool, terminal_workspace: i32) {
-    if create {
-        let mut command: Vec<&str> = ["exec", "[workspace special:rdrop silent;"].to_vec();
-        command.push("float;");
-        command.push("]");
-        command.push(config.terminal.as_str());
-        let class = format!("--class {}", config.class);
-        command.push(class.as_str());
-        let res = dispatch_hyrpctl_command(&*command).expect("TODO: panic message");
-        println!("{}", res)
+    let destination_arg = if ws.id != terminal_workspace {
+        format!("{},", ws.name)
     } else {
-        let mut command: Vec<String> = ["movetoworkspacesilent".to_string()].to_vec();
+        String::from("special:rdrop,")
+    };
 
-        let ws = get_active_workspace().expect("No active workspace found");
-        if ws.id != terminal_workspace {
-            println!("Moved into {}", ws.id);
-            let move_to = format!("{},", ws.name);
-            command.push(move_to);
-        } else {
-            println!("Put in special workspace");
-            command.push("special:rdrop,".to_string())
-        }
+    let command_args: Vec<&str> = vec!["movetoworkspacesilent", &destination_arg, &class_arg];
+    dispatch_hyrpctl_command(&command_args)?;
+    Ok(())
+}
 
-        let class = format!("class:{}", config.class);
-        command.push(class);
+fn dispatch_terminal_resize(
+    class: &str,
+    terminal_size: &TerminalSize,
+) -> Result<(), Box<dyn Error>> {
+    let class_arg = format!("class:{}", class);
+    let width_str = terminal_size.width.to_string();
+    let height_str = terminal_size.height.to_string();
 
-        let command_refs: Vec<&str> = command.iter().map(|s| s.as_str()).collect();
-        let res = dispatch_hyrpctl_command(&command_refs).expect("TODO: panic message");
-        println!("{}", res);
+    let command_args: Vec<&str> = vec![
+        "resizewindowpixel",
+        "exact",
+        &width_str,
+        &height_str,
+        ",",
+        &class_arg,
+    ];
 
-        let (width, height) = calc_terminal_percentage_size(config.width, config.height);
+    dispatch_hyrpctl_command(&command_args)?;
+    Ok(())
+}
 
-        let mut resize_command: Vec<String> = [
-            "resizewindowpixel".to_string(),
-            "exact".to_string(),
-            width.to_string(),
-            height.to_string(),
-            ",".to_string(),
-        ]
-        .to_vec();
-        let class2 = format!("class:{}", config.class);
-        resize_command.push(class2);
-        let resize_command_refs: Vec<&str> = resize_command.iter().map(|s| s.as_str()).collect();
-        dispatch_hyrpctl_command(&resize_command_refs).expect("TODO: panic message");
+fn parse_commands(
+    config: &Config,
+    create: bool,
+    terminal_workspace: Option<i32>,
+) -> Result<(), Box<dyn Error>> {
+    if create {
+        log::info!("Terminal session creating...");
+        dispatch_terminal_init(&config.terminal, &config.class)?;
+    } else {
+        log::info!("Move and resize terminal session...");
 
+        let tws = terminal_workspace.expect("Terminal workspace not found during move");
+        dispatch_terminal_move(&config.class, tws)?;
 
+        let monitor = find_active_monitor()?;
 
-        let active_monitor = find_active_monitor();
-        let monitor = active_monitor.unwrap();
-        let (x_pos, y_pos) = match config.position {
-            TermPosition::T => {
-                let x = (monitor.width - width) / 2;
-                let y = config.gap;
-                (x, y)
-            },
-            TermPosition::R => {
-                let x = config.gap;
-                let y = (monitor.height - height) / 2;
-                (x, y)
-            },
-            TermPosition::B => {
-                let x = (monitor.width - width) / 2;
-                let y = monitor.height - ( height + config.gap);
-                (x, y)
-            },
-            TermPosition::L => {
-                let x = monitor.width - (width + config.gap);
-                let y = (monitor.height - height) / 2;
-                (x, y)
-            }
-        };
+        let terminal_size: TerminalSize =
+            calc_terminal_size_from_percentage(config.width, config.height, &monitor);
 
-        let mut move_command: Vec<String> = [
-            "movewindowpixel".to_string(),
-            "exact".to_string(),
-            x_pos.to_string(),
-            y_pos.to_string(),
-            ",".to_string(),
-        ]
-        .to_vec();
-        let class2 = format!("class:{}", config.class);
-        move_command.push(class2);
-        let move_command_refs: Vec<&str> = move_command.iter().map(|s| s.as_str()).collect();
-        dispatch_hyrpctl_command(&move_command_refs).expect("TODO: panic message");
+        dispatch_terminal_resize(&config.class, &terminal_size)?;
+
+        dispatch_terminal_positioning(
+            &config.class,
+            &monitor,
+            &config.position,
+            config.gap,
+            &terminal_size,
+        )?;
     }
+    Ok(())
 }
 
 fn main() {
+    if std::env::var("RUST_LOG").is_err() {
+        unsafe {
+            std::env::set_var("RUST_LOG", "info");
+        }
+    }
+    env_logger::init();
+
     let args = Args::parse();
     let config: Config = match load_configs(PathBuf::from(args.config)) {
         Ok(config) => config,
         Err(e) => {
-            eprintln!("Failed to load config: {}", e);
+            log::error!("Failed to load config {}", e);
             std::process::exit(1);
         }
     };
 
-    let terminal_client = find_terminal(&config.class);
+    let result = match find_terminal(&config.class) {
+        Ok(Some(client)) => parse_commands(&config, false, Some(client.workspace.id)),
+        Ok(None) => parse_commands(&config, true, None),
+        Err(e) => {
+            log::error!("Errore during terminal find: {}", e);
+            std::process::exit(1);
+        }
+    };
 
-    if terminal_client.is_none() {
-        parse_commands(&config, true, 0);
-    } else {
-        parse_commands(&config, false, terminal_client.unwrap().workspace.id);
+    if let Err(e) = result {
+        log::error!("Fatal error during command dispatch: {}", e);
+        std::process::exit(1);
     }
 }
